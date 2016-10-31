e3fc3402f3e7a8c54b4ffbf695aeb82e466896e0
twekkis <twekkis@gmail.com>
2016-11-01 00:19:40 +0200

    new version

--- index.php
+++ index.php
@@ -19,25 +19,55 @@ $pages = array();
 include ('content.inc');
 ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-        <title>Magnetic - Stunning Responsive HTML5/CSS3 Photography Wensite Template</title>
+        <title>HIFK D1 2004 - Kanada/USA 2017</title>
+        <meta http-equiv="Content-Type">
+        <meta content="text/html">
         <meta charset="utf-8">
-        <meta name="author" content="pixelhint.com">
-        <meta name="description" content="Magnetic is a stunning responsive HTML5/CSS3 photography/portfolio website template"/>
+        <meta name="author" content="">
+        <meta name="description" content=""/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
         <link rel="stylesheet" type="text/css" href="css/reset.css">
-        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <link rel="stylesheet" type="text/css" href="css/subpage_responsive.css">
+        <link rel="stylesheet" type="text/css" href="css/frontpage_responsive.css">
     <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/carouFredSel.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
+    <!--[if lt IE 9]>
+    <script>
+      document.createElement('header');
+      document.createElement('nav');
+      document.createElement('section');
+      document.createElement('social');
+      document.createElement('footer');
+    </script>
+    <![endif]-->
 </head>
 <body>
 
-        <?php
-                include('template_sidebar.inc');
-        ?>
+       <header>
+                <div class="wrapper">
+                        <a href="."><img src="img/pages/transparentlogo.png" alt="logo" class="logo"></a>
+                        <a href="." class="menu_icon" id="menu_icon"></a>
+                        <nav id="nav_menu">
+                                <ul>
+                                        <li><a href=".?0">HIFK D1 2005 Joukkue</a></li>
+                                        <li><a href=".?1">Kanada/Usa pelimatka</a></li>
+                                        <li><a href=".?2">Tue meitä</a></li>
+                                </ul>
+                        </nav>
+<!--
+                        <ul class="social">
+                                <li><a class="fb" href="#"></a></li>
+                                <li><a class="twitter" href="#"></a></li>
+                                <li><a class="gplus" href="#"></a></li>
+                        </ul>
+-->
+                </div>
+        </header><!--  End Header  -->
+
         <section class="main clearfix">
                 <?php
                    if ($page_no != null && $page_no < count($pages) && $pages[$page_no]->content_page != null) {
@@ -48,6 +78,19 @@ include ('content.inc');
                    }
                 ?>
         </section><!-- end main -->
+<!--
+        <footer>
+                <img src="img/olomielilogo_black.png" alt="Olo&Mieli" class="logo_black">
+                <p class="contact">Hyvinvointivalmennus Olo&Mieli - Y-tunnus: 2712069-4 </p>
+                <p class="contact">Yhteystiedot: 040 939 8545 -
+                        niina<img src="img/at.jpg" alt='@'>olomieli<img src="img/dot.jpg" alt='.'>net -
+                        Luontaisterveyskeskus ProHyvinvointi, Laulukuja 4, 00420 Helsinki
+                </p>
+    </footer>--><!--  End Footer  -->
+    <div class=copy>
+        <p class="rights">Web copyright © template: www.pixelhint.com, images: www.pixabay.com, icons: www.freepik.com, www.flaticon.com - All rights reserved</p>
+    </div>
 </body>
 </html>
+
 
--- js/main.js
+++ js/main.js
@@ -1,80 +1,32 @@
 $(document).ready(function(){
 
+    /*  Testimonials Slider  */
+	$('#tslider').carouFredSel({
+            circular : true,
+            infinite: false,
+            responsive : true,
+            scroll : {
+                items : 1,
+                fx : "fade",
+                duration        : 1000,
+                pauseOnHover    : true
+            },
+            pagination: {
+				container:	"#t_navigation",
+				anchorBuilder:	function(nr){
+					return '<a href="#'+nr+'"></a>';
+				}
+			}
+    });
 
 
-    //mobile menu toggling
+    /*  hide-show mobile menu  */
     $("#menu_icon").click(function(){
-        $("header nav ul").toggleClass("show_menu");
-        $("#menu_icon").toggleClass("close_menu");
+        $("#nav_menu").toggleClass("show_menu");
         return false;
     });
 
-    
-
-    //Contact Page Map Centering
-    var hw = $('header').width() + 50;
-    var mw = $('#map').width();
-    var wh = $(window).height();
-    var ww = $(window).width();
-
-    $('#map').css({
-        "max-width" : mw,
-        "height" : wh
-    });
-
-    if(ww>1100){
-         $('#map').css({
-            "margin-left" : hw
-        });
-    }
-
-   
-
-
-
-    //Tooltip
-    $("a").mouseover(function(){
-
-        var attr_title = $(this).attr("data-title");
-
-        if( attr_title == undefined || attr_title == "") return false;
-        
-        $(this).after('<span class="tooltip"></span>');
-
-        var tooltip = $(".tooltip");
-        tooltip.append($(this).data('title'));
-
-         
-        var tipwidth = tooltip.outerWidth();
-        var a_width = $(this).width();
-        var a_hegiht = $(this).height() + 3 + 4;
-
-        //if the tooltip width is smaller than the a/link/parent width
-        if(tipwidth < a_width){
-            tipwidth = a_width;
-            $('.tooltip').outerWidth(tipwidth);
-        }
-
-        var tipwidth = '-' + (tipwidth - a_width)/2;
-        $('.tooltip').css({
-            'left' : tipwidth + 'px',
-            'bottom' : a_hegiht + 'px'
-        }).stop().animate({
-            opacity : 1
-        }, 200);
-       
-
-    });
-
-    $("a").mouseout(function(){
-        var tooltip = $(".tooltip");       
-        tooltip.remove();
-    });
-
-
 });
-
-
 
 
 
